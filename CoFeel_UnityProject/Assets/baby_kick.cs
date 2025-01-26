b5a9e36457875b4cd4f2a462da95d6bf1b71bd3a
MiaWu-design <hongxuanwu@berkeley.edu>
2025-01-26 02:14:31 -0500

    brainwavescript

--- CoFeel_UnityProject/Assets/baby_kick.cs
+++ CoFeel_UnityProject/Assets/baby_kick.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using OpenBCI.Network.Streams;
+
 public class Outer : MonoBehaviour
 {
+    [SerializeField] private EMGStream Stream;
     public ParticleSystem particleSystem;
-    public int detectedValue = 0; 
+    // public int detectedValue = 0; 
     private bool isPlaying = false;
 
     void Update()
     {
-        if (detectedValue == 1 && !isPlaying)
+        if (Stream.Channels[0] >= 0.8 || Stream.Channels[1] >= 0.8 && !isPlaying)
         {
             StartCoroutine(PlayParticleSystem());
+        }
+        else
+        {
+            isPlaying = false;
         }
     }
 
