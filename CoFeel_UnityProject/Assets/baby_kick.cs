fdf98dd02b1194b62032ea5d4a96c38feb766516
MiaWu-design <hongxuanwu@berkeley.edu>
2025-01-26 10:18:58 -0500

    Update baby_kick.cs

--- CoFeel_UnityProject/Assets/baby_kick.cs
+++ CoFeel_UnityProject/Assets/baby_kick.cs
@@ -1,23 +1,50 @@
 using UnityEngine;
 using System.Collections;
 
-public class ExampleClass : MonoBehaviour
+using OpenBCI.Network.Streams;
+
+public class emgKick : MonoBehaviour
 {
+    [SerializeField] private EMGStream Stream;
     public Renderer rend;
+
+    private float timer = 0f;
+    private const float DELAY_TIME = 5f;
 
     void Start()
     {
         rend = GetComponent<Renderer>();
-        rend.enabled = true;
+        rend.enabled = false;
     }
 
     // Toggle the Object's visibility each second.
     void Update()
     {
-        // Find out whether current second is odd or even
-        bool oddeven = Mathf.FloorToInt(Time.time) % 2 == 0;
+        Debug.Log("EMG Reading " + Stream.Channels[0]);
+        
+        // if spike
+        if (Stream.Channels[0] > 0.8 && rend.enabled == false)
+        {
+            rend.enabled = true;
+            timer = DELAY_TIME;
+        }
+        else if (rend.enabled)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                rend.enabled = false;
+            }
+        }
+        // else
+        // {
+        //    rend.enabled = false;
+        // }
+        
+            // Find out whether current second is odd or even
+        // bool oddeven = Mathf.FloorToInt(Time.time) % 2 == 0;
 
         // Enable renderer accordingly
-        rend.enabled = oddeven;
+        // rend.enabled = oddeven;
     }
 }
